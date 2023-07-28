c5e93c8a64e9e594a99e7d17fc95549a85f94f4f
sankarebarri <sankarebarri_dev@yahoo.com>
2023-07-28 08:20:15 +0000

    file input/output

--- file_io/read_names.py
+++ file_io/read_names.py
@@ -1,11 +1,23 @@
-# with open('better_name.txt', 'r') as file:
-#     lines = file.readlines()
+### Reading from files
 
-# for line in lines:
-#     # print(f'hello {line}', end="")
-#     print(f'hello {line.rstrip()}') # same as above
-
-### shorter code
+# simple read
 with open('better_name.txt', 'r') as file:
-    for line in file:
+    lines = file.readlines()
+
+for line in lines:
+    # print(f'hello {line}', end="")
+    print(f'hello {line.rstrip()}') # same as above
+
+## shorter code
+with open('better_name.txt', 'r') as file:
+    for line in sorted(file):
         print(f'hell {line}', end='')
+
+### sorting before reading
+names = []
+with open('better_name.txt') as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names, reverse=True):
+    print(f'hello, {name}')
