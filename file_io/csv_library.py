bad59b56414fc9fb63660808ae885d131f991896
sankarebarri <sankarebarri_dev@yahoo.com>
2023-07-29 16:55:27 +0000

    file i/o

--- file_io/csv_library.py
+++ file_io/csv_library.py
@@ -1,11 +1,25 @@
-# read out from the students.csv
+import csv
+
 students = []
+
+# if the csv file does not have a header
+# with open('students.csv') as file:
+#     reader = csv.reader(file)
+#     # for row in reader:
+#     #     students.append({'name': row[0], 'grade': row[1]})
+#     for name, grade in reader:
+#         students.append({'name': name, 'grade': grade})
+
+# if the csv file has a header
 with open('students.csv') as file:
-    for line in file:
-        name, *grade = line.rstrip().split(',')
-        student = {'name': name, 'grade': grade}
-        students.append(student)
+    reader = csv.DictReader(file)
+    # for row in reader:
+    #     students.append({'name': row[0], 'grade': row[1]})
+    for row in reader:
+        students.append({'name': row['name'], 'grade': row['grade']})
 
 for student in sorted(students, key=lambda student: student['name'], reverse=True):
     print(f"{student['name']} is in {student['grade']}")
-        
+
+
+
