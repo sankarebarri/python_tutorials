bc1239ab334f2fcdf7b7feb8c42e6db78dcb26f7
sankarebarri <sankarebarri_dev@yahoo.com>
2023-08-02 11:01:00 +0000

    python_regex

--- python_regex/validate.py
+++ python_regex/validate.py
@@ -1,9 +1,29 @@
-email = input("What's your email: ").strip()
+# email = input("What's your email: ").strip()
 
-username, domain = email.split('@')
+# username, domain = email.split('@')
 
-# if username and domain.endswith(".edu"):
-if username and "." in domain:
-    print("Valid")
+# # if username and domain.endswith(".edu"):
+# if username and "." in domain:
+#     print("Valid")
+# else:
+#     print("Invalid")
+
+# best method using re
+import re
+
+re_email = input("What's your email: ").strip()
+
+if re.search(r".*@.+\.edu", re_email):
+    print("Email Valid")
 else:
-    print("Invalid")
+    print("Email Invalid")
+
+# def test_email(re_email):
+#     if re_email == 'sankarebarri@yahoo.com':
+#         print('Email Valid')
+#     elif re_email == '@yahoo.com':
+#         print('No character before @')
+#     elif re_email == 'sankarebarri@yahoo.com':
+
+
+
