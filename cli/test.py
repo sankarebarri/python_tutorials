bd2292f98f75904050230fe415cb8bc64d670c8e
sankarebarri <sankarebarri_dev@yahoo.com>
2023-10-30 22:52:51 +0000

    command-line --> argparse

--- cli/test.py
+++ cli/test.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--save")
+parser.add_argument("--save", action="store_true")
 parser.add_argument(
     "--file_name",
     nargs=1
